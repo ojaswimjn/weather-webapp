da26696af8c9b4f0e5d519eaff428a5730d06b52
ojaswimjn <maharjanojaswi573@gmail.com>
2025-09-03 09:28:23 +0545

    Data fetch from open weather api

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,15 @@ const App = () => {
       <WeatherBackground condition={getWeatherCondition()}/>
       <div className='flex items-center justify-center p-6 min-h-screen'>
         <div className='bg-transparent backdrop-filter backdrop-blur-md rounded-xl shadow-2xl p-8 max-w-md text-white w-full border border-white/30 relative z-10'>
-          <h1 className='text-4xl font-extrabold text-center'> Weather</h1>
-          
+          <h1 className='text-4xl font-extrabold text-center'> Weather </h1>
+          <SearchComp setWeather={setWeather}/>
+          <ResultComp/>
         </div>
       </div>
-      <div className='flex'>
+      {/* <div className='flex'>
         <div className='h-14 flex-auto'><SearchComp/></div>
         <div className='h-32 flex-auto'><ResultComp/></div>
-    </div>
+    </div> */}
     </div>
     
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import axios from "axios";
+
+const searchComp = ({setWeather}) => {
+  const [ city, setCity] = useState('')
+  const [ result, setResult] = useState(null);
+  const API_KEY= 'e5811a4e58a88a3c6c1422164cc93862'
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    const data = res.data;
+    setResult(data);
+    if (setWeather) setWeather (data);
+
+    console.log(data)
 
 
-const searchComp = () => {
+    }catch(err){
+      console.error(err);
+    }
+  }
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!!
-      </h1>
+      <form onSubmit={handleSearch} action="" className='flex flex-col relative'>
+        <input value={city} onChange={(e) => setCity(e.target.value)} placeholder='Enter city or country (3 min letters' type="text"
+        className='mb-4 p-3 rounded border border-whitebg-transparent text-white placeholder-white focus:outline-none focus:borser-blue-300 transition duration-300' />
+            <button type='submit' className='bg-purple-700 hover:bg-blue-700 text-white'>
+              Get Weather
+            </button>
+      </form>
+
+      
+
     </div>
   )
 }
